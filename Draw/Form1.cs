c3002ba9ec0e1b0e498adc5e1211bb9c97ade5a3
railr <railr@DESKTOP-SFP2QTJ>
2023-01-21 21:37:57 +0300

    Добавьте файлы проекта.

---
+++ Draw/Form1.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Draw
+{
+    public partial class Form1 : Form
+    {
+        int Heightt = 700;
+        int Widthh = 700;
+        int Square = 50;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Graphics graphics = pictureBox1.CreateGraphics();
+            Pen pen = new Pen(Color.Black, 1f);
+
+            Point[] points = new Point[1000];
+
+            for (int i = 0; i < points.Length; i++)
+            {
+
+                //points[i] = new Point(i, (int)(Math.Sin((double)i/50) * 100 + 200));
+
+                points[i] = new Point(i + (Heightt / 2), ((((int)(2.5f * (i))) * -1) + Heightt) - (Square * 3) - (Heightt / 2));
+
+            }
+            graphics.DrawLines(pen, points);
+            for (int i = Heightt / Square; i >= 0; i--)
+            {
+                if (i == (Heightt / Square) / 2)
+                {
+                    pen.Color = Color.Red;
+                }
+                else
+                {
+                    pen.Color = Color.Black;
+                }
+                graphics.DrawLine(pen, 0, i * Square, Widthh, i * Square);
+            }
+            for (int i = Widthh / Square; i >= 0; i--)
+            {
+                if (i == (Heightt / Square) / 2)
+                {
+                    pen.Color = Color.Red;
+                }
+                else
+                {
+                    pen.Color = Color.Black;
+                }
+                graphics.DrawLine(pen, i * Square, 0, i * Square, Heightt);
+            }
+        }
+    }
+}
---
+++ Draw/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// Общие сведения об этой сборке предоставляются следующим набором
+// набора атрибутов. Измените значения этих атрибутов для изменения сведений,
+// связанных со сборкой.
+[assembly: AssemblyTitle("Draw")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Draw")]
+[assembly: AssemblyCopyright("Copyright ©  2023")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Установка значения False для параметра ComVisible делает типы в этой сборке невидимыми
+// для компонентов COM. Если необходимо обратиться к типу в этой сборке через
+// COM, следует установить атрибут ComVisible в TRUE для этого типа.
+[assembly: ComVisible(false)]
+
+// Следующий GUID служит для идентификации библиотеки типов, если этот проект будет видимым для COM
+[assembly: Guid("af62ca05-96bc-464f-8ca8-bf2bc84d4d34")]
+
+// Сведения о версии сборки состоят из указанных ниже четырех значений:
+//
+//      Основной номер версии
+//      Дополнительный номер версии
+//      Номер сборки
+//      Редакция
+//
+// Можно задать все значения или принять номера сборки и редакции по умолчанию 
+// используя "*", как показано ниже:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
