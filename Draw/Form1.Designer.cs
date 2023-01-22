74495d13a648ea3916c0b97a6e252748e48cad37
Rail Rahimov <railra498@gmail.com>
2023-01-22 11:52:51 +0300

    Созданы линейный график, поле

--- Draw/Form1.Designer.cs
+++ Draw/Form1.Designer.cs
@@ -37,7 +37,7 @@
             // 
             this.pictureBox1.Location = new System.Drawing.Point(12, 12);
             this.pictureBox1.Name = "pictureBox1";
-            this.pictureBox1.Size = new System.Drawing.Size(1000, 1000);
+            this.pictureBox1.Size = new System.Drawing.Size(1000, 875);
             this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.CenterImage;
             this.pictureBox1.TabIndex = 0;
             this.pictureBox1.TabStop = false;
@@ -61,7 +61,6 @@
             this.Controls.Add(this.pictureBox1);
             this.Name = "Form1";
             this.Text = "Form1";
-            this.TopMost = true;
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
             this.ResumeLayout(false);
 
