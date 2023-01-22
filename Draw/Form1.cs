9cb8cc6c23d7b8717ec4a3dd7d740f860e152fb5
Rail Rahimov <railra498@gmail.com>
2023-01-22 12:57:06 +0300

    Добавлена правильная парабола

--- Draw/Form1.cs
+++ Draw/Form1.cs
@@ -28,8 +28,19 @@ namespace Draw
                 double y;
                 points[i] = new Point((i) + (Widthh / 2), (int)((((2.5f * (i))) * -1)) - (Square * 3) + (Heightt / 2));
                 points2[i] = new Point((i - 999) + (Widthh / 2), (int)((((2.5f * (i - 999))) * -1)) - (Square * 3) + (Heightt / 2));
-                points3[i] = new Point((i) + (Widthh / 2), (((i * i) - 3 * i) * -1) - (9 * Square) + (Heightt / 2));
-                points4[i] = new Point((i - 999) + (Widthh / 2), ((((i - 999) * (i - 999)) - 3 * (i - 999)) * -1) - (9 * Square) + (Heightt / 2));
+            }
+            double g = 0.0;
+            for (int i = 0; i < points.Length; i++)
+            {
+
+                points3[i] = new Point((i) + (Widthh / 2), (int)(((((g * g) - 3 * g) * -1) - 9) * Square + (Heightt / 2)));
+                points4[i] = new Point((i - 999) + (Widthh / 2), (int)((((((g - 99.9) * (g - 99.9)) - 3 * (g - 99.9)) * -1) - 9) * Square + (Heightt / 2)));
+                if (i == 2)
+                {
+                    button1.Text = ((((i * i) - 3 * i) * -1) - (9 * Square) + (Heightt / 2)).ToString();
+                }
+                g += 0.1;
+
             }
             graphics.DrawLines(pen, points);
             graphics.DrawLines(pen, points2);
