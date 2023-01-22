024b06c2875fa2f75ccf508f79d78cccf1ec5f11
Rail Rahimov <railra498@gmail.com>
2023-01-22 17:13:09 +0300

    Добавлены координаты

--- Draw/Form1.Designer.cs
+++ Draw/Form1.Designer.cs
@@ -30,38 +30,135 @@
         {
             this.pictureBox1 = new System.Windows.Forms.PictureBox();
             this.button1 = new System.Windows.Forms.Button();
+            this.radioButton1 = new System.Windows.Forms.RadioButton();
+            this.radioButton2 = new System.Windows.Forms.RadioButton();
+            this.radioButton3 = new System.Windows.Forms.RadioButton();
+            this.radioButton4 = new System.Windows.Forms.RadioButton();
+            this.radioButton5 = new System.Windows.Forms.RadioButton();
+            this.radioButton6 = new System.Windows.Forms.RadioButton();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.groupBox1.SuspendLayout();
             this.SuspendLayout();
             // 
             // pictureBox1
             // 
+            this.pictureBox1.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
             this.pictureBox1.Location = new System.Drawing.Point(12, 12);
             this.pictureBox1.Name = "pictureBox1";
-            this.pictureBox1.Size = new System.Drawing.Size(1200, 1000);
+            this.pictureBox1.Size = new System.Drawing.Size(1122, 1000);
             this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.CenterImage;
             this.pictureBox1.TabIndex = 0;
             this.pictureBox1.TabStop = false;
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(418, 582);
+            this.button1.Location = new System.Drawing.Point(1155, 103);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(215, 51);
             this.button1.TabIndex = 1;
-            this.button1.Text = "button1";
+            this.button1.Text = "Нарисовать";
             this.button1.UseVisualStyleBackColor = true;
             this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // radioButton1
+            // 
+            this.radioButton1.AutoSize = true;
+            this.radioButton1.Checked = true;
+            this.radioButton1.Location = new System.Drawing.Point(6, 21);
+            this.radioButton1.Name = "radioButton1";
+            this.radioButton1.Size = new System.Drawing.Size(35, 20);
+            this.radioButton1.TabIndex = 2;
+            this.radioButton1.TabStop = true;
+            this.radioButton1.Text = "5";
+            this.radioButton1.UseVisualStyleBackColor = true;
+            this.radioButton1.UseWaitCursor = true;
+            // 
+            // radioButton2
+            // 
+            this.radioButton2.AutoSize = true;
+            this.radioButton2.Location = new System.Drawing.Point(75, 21);
+            this.radioButton2.Name = "radioButton2";
+            this.radioButton2.Size = new System.Drawing.Size(42, 20);
+            this.radioButton2.TabIndex = 3;
+            this.radioButton2.Text = "10";
+            this.radioButton2.UseVisualStyleBackColor = true;
+            this.radioButton2.UseWaitCursor = true;
+            // 
+            // radioButton3
+            // 
+            this.radioButton3.AutoSize = true;
+            this.radioButton3.Location = new System.Drawing.Point(145, 21);
+            this.radioButton3.Name = "radioButton3";
+            this.radioButton3.Size = new System.Drawing.Size(42, 20);
+            this.radioButton3.TabIndex = 4;
+            this.radioButton3.Text = "20";
+            this.radioButton3.UseVisualStyleBackColor = true;
+            this.radioButton3.UseWaitCursor = true;
+            // 
+            // radioButton4
+            // 
+            this.radioButton4.AutoSize = true;
+            this.radioButton4.Location = new System.Drawing.Point(6, 47);
+            this.radioButton4.Name = "radioButton4";
+            this.radioButton4.Size = new System.Drawing.Size(42, 20);
+            this.radioButton4.TabIndex = 5;
+            this.radioButton4.Text = "40";
+            this.radioButton4.UseVisualStyleBackColor = true;
+            this.radioButton4.UseWaitCursor = true;
+            // 
+            // radioButton5
+            // 
+            this.radioButton5.AutoSize = true;
+            this.radioButton5.Location = new System.Drawing.Point(75, 47);
+            this.radioButton5.Name = "radioButton5";
+            this.radioButton5.Size = new System.Drawing.Size(42, 20);
+            this.radioButton5.TabIndex = 6;
+            this.radioButton5.Text = "50";
+            this.radioButton5.UseVisualStyleBackColor = true;
+            this.radioButton5.UseWaitCursor = true;
+            // 
+            // radioButton6
+            // 
+            this.radioButton6.AutoSize = true;
+            this.radioButton6.Location = new System.Drawing.Point(145, 47);
+            this.radioButton6.Name = "radioButton6";
+            this.radioButton6.Size = new System.Drawing.Size(49, 20);
+            this.radioButton6.TabIndex = 7;
+            this.radioButton6.Text = "100";
+            this.radioButton6.UseVisualStyleBackColor = true;
+            this.radioButton6.UseWaitCursor = true;
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.radioButton1);
+            this.groupBox1.Controls.Add(this.radioButton6);
+            this.groupBox1.Controls.Add(this.radioButton2);
+            this.groupBox1.Controls.Add(this.radioButton5);
+            this.groupBox1.Controls.Add(this.radioButton3);
+            this.groupBox1.Controls.Add(this.radioButton4);
+            this.groupBox1.Location = new System.Drawing.Point(1155, 23);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(215, 74);
+            this.groupBox1.TabIndex = 8;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Размер клеток";
+            this.groupBox1.UseWaitCursor = true;
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(1382, 955);
+            this.Controls.Add(this.groupBox1);
             this.Controls.Add(this.button1);
             this.Controls.Add(this.pictureBox1);
             this.Name = "Form1";
             this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
             this.ResumeLayout(false);
 
         }
@@ -70,6 +167,13 @@
 
         private System.Windows.Forms.PictureBox pictureBox1;
         private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.RadioButton radioButton1;
+        private System.Windows.Forms.RadioButton radioButton2;
+        private System.Windows.Forms.RadioButton radioButton3;
+        private System.Windows.Forms.RadioButton radioButton4;
+        private System.Windows.Forms.RadioButton radioButton5;
+        private System.Windows.Forms.RadioButton radioButton6;
+        private System.Windows.Forms.GroupBox groupBox1;
     }
 }
 
--- Draw/Form1.cs
+++ Draw/Form1.cs
@@ -8,46 +8,70 @@ namespace Draw
     {
         int heightField = 800;
         int widthField = 800;
-        int square = 20;
-        
+        int square = 40;
+        int xxxx = 1;
 
         public Form1()
         {
             InitializeComponent();
-           
         }
 
         private void button1_Click(object sender, EventArgs e)
         {
+
             int middleOfFieldInHeight = heightField / 2;
             int middleOfFieldInWidth = widthField / 2;
-            float neg = 999/square;
             Graphics graphics = pictureBox1.CreateGraphics();
+            graphics.Clear(Color.White);
+            int number = 0;
+
+            if (radioButton1.Checked == true)
+            {
+                square = Int32.Parse(radioButton1.Text);
+                number = 10;
+            }
+            else if (radioButton2.Checked == true)
+            {
+                square = Int32.Parse(radioButton2.Text);
+                number = 5;
+            }
+            else if (radioButton3.Checked == true)
+            {
+                square = Int32.Parse(radioButton3.Text);
+                number = 4;
+            }
+            else if (radioButton4.Checked == true)
+            {
+                square = Int32.Parse(radioButton4.Text);
+                number = 2;
+            }
+            else if (radioButton5.Checked == true)
+            {
+                square = Int32.Parse(radioButton5.Text);
+                number = 2;
+            }
+            else
+            {
+                square = Int32.Parse(radioButton6.Text);
+                number = 1;
+            }
+            using (Font myFont = new Font("Arial", 14))
+            {
+                graphics.DrawString("O", myFont, Brushes.Red, new Point(middleOfFieldInWidth, middleOfFieldInHeight));
+                for (int i = 1; i <= 10; i++)
+                {
+                    graphics.DrawString((number * i).ToString(), myFont, Brushes.Blue, new Point(middleOfFieldInWidth + square * (number * i), middleOfFieldInHeight));
+                    graphics.DrawString((number * -i).ToString(), myFont, Brushes.Blue, new Point(middleOfFieldInWidth, middleOfFieldInHeight + square * (number * i)));
+                    graphics.DrawString((number * -i).ToString(), myFont, Brushes.Blue, new Point(middleOfFieldInWidth - square * (number * i), middleOfFieldInHeight));
+                    graphics.DrawString((number * i).ToString(), myFont, Brushes.Blue, new Point(middleOfFieldInWidth, middleOfFieldInHeight - square * (number * i)));
+                }
+
+            }
+
+            float neg = 999 / square;
             Pen pen = new Pen(Color.Black, 1f);
 
-            Point[] points = new Point[1000];
-            Point[] points2 = new Point[1000];
-            Point[] points3 = new Point[1000];
-            Point[] points4 = new Point[1000];
-            double l = 0f / square;
-            for (int i = 0; i < points.Length; i++)
-            {
-                points[i] = new Point((i) + middleOfFieldInWidth, (int)((((((2.5f * (l))) * -1)) - 3) * square + middleOfFieldInHeight));
-                points2[i] = new Point((i - (1000-square)) + middleOfFieldInWidth, (int)((((((2.5f * (l - neg))) * -1)) - 3) * (square) + middleOfFieldInHeight));
-                l += (1f / square);
-            }
-            double g = 0f / square;
-            for (int i = 0; i < points.Length; i++)
-            {
-                points3[i] = new Point((i) + middleOfFieldInWidth, (int)(((((g * g) - 3 * g) * -1) - 9) * square + middleOfFieldInHeight));
-                points4[i] = new Point((i - (1000-square)) + middleOfFieldInWidth, (int)((((((g - neg) * (g - neg)) - 3 * (g - neg)) * -1) - 9) * square + middleOfFieldInHeight));
-                g += (1f / square);
 
-            }
-            graphics.DrawLines(pen, points);
-            graphics.DrawLines(pen, points2);
-            graphics.DrawLines(pen, points3);
-            graphics.DrawLines(pen, points4);
 
             for (int i = heightField / square; i >= 0; i--)
             {
@@ -77,6 +101,38 @@ namespace Draw
                 }
                 graphics.DrawLine(pen, i * square, 0, i * square, heightField);
             }
+
+            Point[] points = new Point[1000];
+            Point[] points2 = new Point[1000];
+            Point[] points3 = new Point[1000];
+            Point[] points4 = new Point[1000];
+            double l = 0f / square;
+            for (int i = 0; i < points.Length; i++)
+            {
+                points[i] = new Point((i) + middleOfFieldInWidth, (int)((((((2.5f * (l))) * -1)) - 3) * square + middleOfFieldInHeight));
+                points2[i] = new Point((i - (1000 - square)) + middleOfFieldInWidth, (int)((((((2.5f * (l - neg))) * -1)) - 3) * (square) + middleOfFieldInHeight));
+                l += (1f / square);
+            }
+            double g = 0f / square;
+            for (int i = 0; i < points.Length; i++)
+            {
+                points3[i] = new Point((i) + middleOfFieldInWidth, (int)(((((g * g) - 3 * g) * -1) - 9) * square + middleOfFieldInHeight));
+                points4[i] = new Point((i - (1000 - square)) + middleOfFieldInWidth, (int)((((((g - neg) * (g - neg)) - 3 * (g - neg)) * -1) - 9) * square + middleOfFieldInHeight));
+                g += (1f / square);
+
+            }
+            graphics.DrawLines(pen, points);
+            graphics.DrawLines(pen, points2);
+            graphics.DrawLines(pen, points3);
+            graphics.DrawLines(pen, points4);
+
         }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+
     }
 }
