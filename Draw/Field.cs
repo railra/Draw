79e976e7073c1c459c21c55088cd3992dac83c9d
Rail Rahimov <railra498@gmail.com>
2023-01-22 19:04:22 +0300

    Класс "Поле"

---
+++ Draw/Field.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Draw
+{
+    internal class Field
+    {
+        public int cellSize;
+        public int heightField = 800;
+        public int widthField = 800;
+        public void Grid(Graphics graphics, Pen pen, int interval)
+        {
+            for (int i = heightField / cellSize; i >= 0; i--)
+            {
+                if (i == (heightField / cellSize) / 2)
+                {
+                    pen.Color = Color.Red;
+                    pen.Width = 2f;
+                }
+                else
+                {
+                    pen.Color = Color.Black;
+                    pen.Width = 1f;
+                }
+                graphics.DrawLine(pen, 0, i * cellSize, widthField, i * cellSize);
+            }
+            for (int i = widthField / cellSize; i >= 0; i--)
+            {
+                if (i == (widthField / cellSize) / 2)
+                {
+                    pen.Color = Color.Red;
+                    pen.Width = 2f;
+                }
+                else
+                {
+                    pen.Color = Color.Black;
+                    pen.Width = 1f;
+                }
+                graphics.DrawLine(pen, i * cellSize, 0, i * cellSize, heightField);
+            }
+            using (Font myFont = new Font("Arial", 12, FontStyle.Bold))
+            {
+                int sizeOfInteval;
+                graphics.DrawString("O", myFont, Brushes.Red, new Point(widthField / 2, heightField / 2));
+                for (int i = 1; i <= 10; i++)
+                {
+                    sizeOfInteval = cellSize * (interval * i);
+                    graphics.DrawString((interval * i).ToString(), myFont, Brushes.Blue, new Point(widthField / 2 + sizeOfInteval, heightField / 2));
+                    graphics.DrawString((interval * -i).ToString(), myFont, Brushes.Blue, new Point(widthField / 2, heightField / 2 + sizeOfInteval));
+                    graphics.DrawString((interval * -i).ToString(), myFont, Brushes.Blue, new Point(widthField / 2 - sizeOfInteval, heightField / 2));
+                    graphics.DrawString((interval * i).ToString(), myFont, Brushes.Blue, new Point(widthField / 2, heightField / 2 - sizeOfInteval));
+                }
+            }
+        }
+    }
+}
--- Draw/Form1.cs
+++ Draw/Form1.cs
@@ -6,9 +6,7 @@ namespace Draw
 {
     public partial class Form1 : Form
     {
-        int heightField = 800;
-        int widthField = 800;
-        int cellSize;
+        Field field = new Field();
 
         public Form1()
         {
@@ -17,92 +15,54 @@ namespace Draw
 
         private void Button1_Click(object sender, EventArgs e)
         {
-            int middleOfFieldInHeight = heightField / 2;
-            int middleOfFieldInWidth = widthField / 2;
+            int middleOfFieldInHeight = field.heightField / 2;
+            int middleOfFieldInWidth = field.widthField / 2;
             Graphics graphics = pictureBox1.CreateGraphics();
             graphics.Clear(Color.White);
+            Pen pen = new Pen(Color.Black, 1f);
             int interval;
             if (radioButton1.Checked == true)
             {
-                cellSize = Int32.Parse(radioButton1.Text);
+                field.cellSize = Int32.Parse(radioButton1.Text);
                 interval = 10;
             }
             else if (radioButton2.Checked == true)
             {
-                cellSize = Int32.Parse(radioButton2.Text);
+                field.cellSize = Int32.Parse(radioButton2.Text);
                 interval = 5;
             }
             else if (radioButton3.Checked == true)
             {
-                cellSize = Int32.Parse(radioButton3.Text);
+                field.cellSize = Int32.Parse(radioButton3.Text);
                 interval = 4;
             }
             else if (radioButton4.Checked == true)
             {
-                cellSize = Int32.Parse(radioButton4.Text);
+                field.cellSize = Int32.Parse(radioButton4.Text);
                 interval = 2;
             }
             else if (radioButton5.Checked == true)
             {
-                cellSize = Int32.Parse(radioButton5.Text);
+                field.cellSize = Int32.Parse(radioButton5.Text);
                 interval = 2;
             }
             else
             {
-                cellSize = Int32.Parse(radioButton6.Text);
+                field.cellSize = Int32.Parse(radioButton6.Text);
                 interval = 1;
             }
-            using (Font myFont = new Font("Arial", 12, FontStyle.Bold))
-            {
-                int sizeOfInteval;
-                graphics.DrawString("O", myFont, Brushes.Red, new Point(middleOfFieldInWidth, middleOfFieldInHeight));
-                for (int i = 1; i <= 10; i++)
-                {
-                    sizeOfInteval = cellSize * (interval * i);
-                    graphics.DrawString((interval * i).ToString(), myFont, Brushes.Blue, new Point(middleOfFieldInWidth + sizeOfInteval, middleOfFieldInHeight));
-                    graphics.DrawString((interval * -i).ToString(), myFont, Brushes.Blue, new Point(middleOfFieldInWidth, middleOfFieldInHeight + sizeOfInteval));
-                    graphics.DrawString((interval * -i).ToString(), myFont, Brushes.Blue, new Point(middleOfFieldInWidth - sizeOfInteval, middleOfFieldInHeight));
-                    graphics.DrawString((interval * i).ToString(), myFont, Brushes.Blue, new Point(middleOfFieldInWidth, middleOfFieldInHeight - sizeOfInteval));
-                }
-            }
+            
 
-            Pen pen = new Pen(Color.Black, 1f);
+            
 
-            for (int i = heightField / cellSize; i >= 0; i--)
-            {
-                if (i == (heightField / cellSize) / 2)
-                {
-                    pen.Color = Color.Red;
-                    pen.Width = 2f;
-                }
-                else
-                {
-                    pen.Color = Color.Black;
-                    pen.Width = 1f;
-                }
-                graphics.DrawLine(pen, 0, i * cellSize, widthField, i * cellSize);
-            }
-            for (int i = widthField / cellSize; i >= 0; i--)
-            {
-                if (i == (widthField / cellSize) / 2)
-                {
-                    pen.Color = Color.Red;
-                    pen.Width = 2f;
-                }
-                else
-                {
-                    pen.Color = Color.Black;
-                    pen.Width = 1f;
-                }
-                graphics.DrawLine(pen, i * cellSize, 0, i * cellSize, heightField);
-            }
+            field.Grid(graphics, pen, interval);
 
             Point[] points = new Point[1000];
             Point[] points2 = new Point[1000];
             Point[] points3 = new Point[1000];
             Point[] points4 = new Point[1000];
             double l = 0;
-            float neg = 999 / cellSize;
+            float neg = 999 / field.cellSize;
             double function1Right;
             double function1Left;
             double function2Right;
@@ -111,9 +71,9 @@ namespace Draw
             {
                 function1Right = ((2.5f * l)) * -1 - 3;
                 function1Left = (2.5f * (l - neg)) * -1 - 3;
-                points[i] = new Point((i) + middleOfFieldInWidth, (int)(function1Right * cellSize + middleOfFieldInHeight));
-                points2[i] = new Point((i - (1000 - cellSize)) + middleOfFieldInWidth, (int)(function1Left * (cellSize) + middleOfFieldInHeight));
-                l += (1f / cellSize);
+                points[i] = new Point((i) + middleOfFieldInWidth, (int)(function1Right * field.cellSize + middleOfFieldInHeight));
+                points2[i] = new Point((i - (1000 - field.cellSize)) + middleOfFieldInWidth, (int)(function1Left * (field.cellSize) + middleOfFieldInHeight));
+                l += (1f / field.cellSize);
             }
 
             double g = 0;
@@ -121,9 +81,9 @@ namespace Draw
             {
                 function2Right = ((g * g - 3 * g) * -1 - 9);
                 function2Left = ((g - neg) * (g - neg) - 3 * (g - neg)) * -1 - 9;
-                points3[i] = new Point((i) + middleOfFieldInWidth, (int)(function2Right * cellSize + middleOfFieldInHeight));
-                points4[i] = new Point((i - (1000 - cellSize)) + middleOfFieldInWidth, (int)(function2Left * cellSize + middleOfFieldInHeight));
-                g += (1f / cellSize);
+                points3[i] = new Point((i) + middleOfFieldInWidth, (int)(function2Right * field.cellSize + middleOfFieldInHeight));
+                points4[i] = new Point((i - (1000 - field.cellSize)) + middleOfFieldInWidth, (int)(function2Left * field.cellSize + middleOfFieldInHeight));
+                g += (1f / field.cellSize);
             }
 
             pen.Color = Color.Green;
