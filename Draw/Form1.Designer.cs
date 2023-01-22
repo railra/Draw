703fc2d02a9bb511833e39b74489885c1c55aea1
Rail Rahimov <railra498@gmail.com>
2023-01-22 15:52:40 +0300

    Рабочее масшатабирование

--- Draw/Form1.Designer.cs
+++ Draw/Form1.Designer.cs
@@ -37,7 +37,7 @@
             // 
             this.pictureBox1.Location = new System.Drawing.Point(12, 12);
             this.pictureBox1.Name = "pictureBox1";
-            this.pictureBox1.Size = new System.Drawing.Size(1000, 875);
+            this.pictureBox1.Size = new System.Drawing.Size(1200, 1000);
             this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.CenterImage;
             this.pictureBox1.TabIndex = 0;
             this.pictureBox1.TabStop = false;
--- Draw/Form1.cs
+++ Draw/Form1.cs
@@ -6,16 +6,22 @@ namespace Draw
 {
     public partial class Form1 : Form
     {
-        int Heightt = 700;
-        int Widthh = 700;
-        int Square = 10;
+        int heightField = 800;
+        int widthField = 800;
+        int square = 20;
+        
+
         public Form1()
         {
             InitializeComponent();
+           
         }
 
         private void button1_Click(object sender, EventArgs e)
         {
+            int middleOfFieldInHeight = heightField / 2;
+            int middleOfFieldInWidth = widthField / 2;
+            float neg = 999/square;
             Graphics graphics = pictureBox1.CreateGraphics();
             Pen pen = new Pen(Color.Black, 1f);
 
@@ -23,52 +29,53 @@ namespace Draw
             Point[] points2 = new Point[1000];
             Point[] points3 = new Point[1000];
             Point[] points4 = new Point[1000];
+            double l = 0f / square;
             for (int i = 0; i < points.Length; i++)
             {
-                double y;
-                points[i] = new Point((i) + (Widthh / 2), (int)((((2.5f * (i))) * -1)) - (Square * 3) + (Heightt / 2));
-                points2[i] = new Point((i - 999) + (Widthh / 2), (int)((((2.5f * (i - 999))) * -1)) - (Square * 3) + (Heightt / 2));
+                points[i] = new Point((i) + middleOfFieldInWidth, (int)((((((2.5f * (l))) * -1)) - 3) * square + middleOfFieldInHeight));
+                points2[i] = new Point((i - (1000-square)) + middleOfFieldInWidth, (int)((((((2.5f * (l - neg))) * -1)) - 3) * (square) + middleOfFieldInHeight));
+                l += (1f / square);
             }
-            double g = 0.0;
+            double g = 0f / square;
             for (int i = 0; i < points.Length; i++)
             {
-
-                points3[i] = new Point((i) + (Widthh / 2), (int)(((((g * g) - 3 * g) * -1) - 9) * Square + (Heightt / 2)));
-                points4[i] = new Point((i - 999) + (Widthh / 2), (int)((((((g - 99.9) * (g - 99.9)) - 3 * (g - 99.9)) * -1) - 9) * Square + (Heightt / 2)));
-                if (i == 2)
-                {
-                    button1.Text = ((((i * i) - 3 * i) * -1) - (9 * Square) + (Heightt / 2)).ToString();
-                }
-                g += 0.1;
+                points3[i] = new Point((i) + middleOfFieldInWidth, (int)(((((g * g) - 3 * g) * -1) - 9) * square + middleOfFieldInHeight));
+                points4[i] = new Point((i - (1000-square)) + middleOfFieldInWidth, (int)((((((g - neg) * (g - neg)) - 3 * (g - neg)) * -1) - 9) * square + middleOfFieldInHeight));
+                g += (1f / square);
 
             }
             graphics.DrawLines(pen, points);
             graphics.DrawLines(pen, points2);
             graphics.DrawLines(pen, points3);
             graphics.DrawLines(pen, points4);
-            for (int i = Heightt / Square; i >= 0; i--)
+
+            for (int i = heightField / square; i >= 0; i--)
             {
-                if (i == (Heightt / Square) / 2)
+                if (i == (heightField / square) / 2)
                 {
                     pen.Color = Color.Red;
+                    pen.Width = 2f;
                 }
                 else
                 {
                     pen.Color = Color.Black;
+                    pen.Width = 1f;
                 }
-                graphics.DrawLine(pen, 0, i * Square, Widthh, i * Square);
+                graphics.DrawLine(pen, 0, i * square, widthField, i * square);
             }
-            for (int i = Widthh / Square; i >= 0; i--)
+            for (int i = widthField / square; i >= 0; i--)
             {
-                if (i == (Widthh / Square) / 2)
+                if (i == (widthField / square) / 2)
                 {
                     pen.Color = Color.Red;
+                    pen.Width = 2f;
                 }
                 else
                 {
                     pen.Color = Color.Black;
+                    pen.Width = 1f;
                 }
-                graphics.DrawLine(pen, i * Square, 0, i * Square, Heightt);
+                graphics.DrawLine(pen, i * square, 0, i * square, heightField);
             }
         }
     }
